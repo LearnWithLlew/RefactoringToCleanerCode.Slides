04f6d0267ec6928821333b768b2f563afe15f56e
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-12-29 09:53:05 -0700

    Create sync_with_one_drive.sh

---
+++ sync_with_one_drive.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+cp /Users/llewellynfalco/Documents/Presentations/RefactoringToCleanerCode.Slides/RefactoringToCleanerCode.pptx  /Users/llewellynfalco/OneDrive/RefactoringToCleanerCode.pptx 
